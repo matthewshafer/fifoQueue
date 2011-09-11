f0fcc8033b899d27881fc2ec1477430b71146603
Matthew Shafer <matthewshafer@mac.com>
2011-09-10 20:29:27 -0700

    some cool stuff

--- tests/memoryUsage.php
+++ tests/memoryUsage.php
@@ -6,6 +6,11 @@ $queue = new fifoQueue();
 
 $i = 0;
 
+$mtime = microtime(); 
+$mtime = explode(" ",$mtime); 
+$mtime = $mtime[1] + $mtime[0]; 
+$starttime = $mtime;
+
 $first = memory_get_usage();
 echo $first . "\n";
 
@@ -19,7 +24,21 @@ $second = memory_get_usage();
 
 echo $second . "\n";
 
+echo $second - $first . "\n";
+
+$mtime = microtime(); 
+$mtime = explode(" ",$mtime); 
+$mtime = $mtime[1] + $mtime[0]; 
+$endtime = $mtime; 
+$totaltime = ($endtime - $starttime); 
+echo $totaltime." seconds\n";
+
 $i = 0;
+
+$mtime = microtime(); 
+$mtime = explode(" ",$mtime); 
+$mtime = $mtime[1] + $mtime[0]; 
+$starttime = $mtime;
 
 while($i < 1000)
 {
@@ -29,5 +48,126 @@ while($i < 1000)
 
 $third = memory_get_usage();
 echo $third . "\n";
+
+$mtime = microtime(); 
+$mtime = explode(" ",$mtime); 
+$mtime = $mtime[1] + $mtime[0]; 
+$endtime = $mtime; 
+$totaltime = ($endtime - $starttime); 
+echo $totaltime." seconds\n";
+
+$queue = null;
+
+$queue = new SplQueue();
+
+$i = 0;
+
+$mtime = microtime(); 
+$mtime = explode(" ",$mtime); 
+$mtime = $mtime[1] + $mtime[0]; 
+$starttime = $mtime;
+
+$first = memory_get_usage();
+
+while($i < 1000)
+{
+	$queue->enqueue($i);
+	$i++;
+}
+
+$second = memory_get_usage();
+
+echo $second . "\n";
+
+echo $second - $first . "\n";
+
+$mtime = microtime(); 
+$mtime = explode(" ",$mtime); 
+$mtime = $mtime[1] + $mtime[0]; 
+$endtime = $mtime; 
+$totaltime = ($endtime - $starttime); 
+echo $totaltime." seconds\n";
+
+$i = 0;
+
+$mtime = microtime(); 
+$mtime = explode(" ",$mtime); 
+$mtime = $mtime[1] + $mtime[0]; 
+$starttime = $mtime;
+
+while($i < 1000)
+{
+	$queue->dequeue();
+	$i++;
+}
+
+$third = memory_get_usage();
+echo $third . "\n";
+
+$mtime = microtime(); 
+$mtime = explode(" ",$mtime); 
+$mtime = $mtime[1] + $mtime[0]; 
+$endtime = $mtime; 
+$totaltime = ($endtime - $starttime); 
+echo $totaltime." seconds\n";
+
+
+$queue = null;
+
+$queue = array();
+
+$i = 0;
+
+$mtime = microtime(); 
+$mtime = explode(" ",$mtime); 
+$mtime = $mtime[1] + $mtime[0]; 
+$starttime = $mtime;
+
+$first = memory_get_usage();
+
+while($i < 1000)
+{
+	$queu[] = $i;
+	$i++;
+}
+
+$second = memory_get_usage();
+
+echo $second . "\n";
+
+echo $second - $first . "\n";
+
+$mtime = microtime(); 
+$mtime = explode(" ",$mtime); 
+$mtime = $mtime[1] + $mtime[0]; 
+$endtime = $mtime; 
+$totaltime = ($endtime - $starttime); 
+echo $totaltime." seconds\n";
+
+$i = 0;
+
+$mtime = microtime(); 
+$mtime = explode(" ",$mtime); 
+$mtime = $mtime[1] + $mtime[0]; 
+$starttime = $mtime;
+
+
+while($i < 1000)
+{
+	array_shift($queue);
+	$i++;
+}
+
+$third = memory_get_usage();
+echo $third . "\n";
+
+$mtime = microtime(); 
+$mtime = explode(" ",$mtime); 
+$mtime = $mtime[1] + $mtime[0]; 
+$endtime = $mtime; 
+$totaltime = ($endtime - $starttime); 
+echo $totaltime." seconds\n";
+
+
 
 ?>
